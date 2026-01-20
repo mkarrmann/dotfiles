0863743cfe06e7f4e6e084521afb8315474fe358
Matt Karrmann <mattkarrmann@gmail.com>
2026-01-19 19:12:32 -0800

    fix init script-havent actually tested, just let chatgpt write it haha

--- init.sh
+++ init.sh
@@ -1,30 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-ln -s .bash_aliases ~/.bash_aliases
-ln -s .bashrc ~/.bashrc
-ln -s .screenrc ~/.screenrc
-ln -s .inputrc ~/.inputrc
-ln -s .bash_profile ~/.bash_profile
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .git-prompt.sh ~/.git-prompt.sh
+DOTFILES_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+link_one() {
+  local src="$1"
+  local dst="$2"
+
+  if [[ ! -e "$src" ]]; then
+    echo "ERROR: source missing: $src" >&2
+    exit 1
+  fi
+
+  # Fail if destination already exists (file/dir/symlink, including broken symlink)
+  if [[ -e "$dst" || -L "$dst" ]]; then
+    echo "ERROR: destination already exists: $dst" >&2
+    exit 1
+  fi
+
+  ln -s "$src" "$dst"
+  echo "linked $dst -> $src"
+}
+
+# Top-level dotfiles
+for f in \
+  .bash_aliases \
+  .bashrc \
+  .screenrc \
+  .inputrc \
+  .bash_profile \
+  .tmux.conf \
+  .git-prompt.sh
+do
+  link_one "$DOTFILES_DIR/$f" "$HOME/$f"
+done
+
+# Neovim
+mkdir -p "$HOME/.config/nvim"
+link_one "$DOTFILES_DIR/nvim_init.lua" "$HOME/.config/nvim/init.lua"
+
+# ~/bin (link each file individually; fail if any target exists)
+mkdir -p "$HOME/bin"
+shopt -s nullglob
+for src in "$DOTFILES_DIR/bin/"*; do
+  base="$(basename "$src")"
+  link_one "$src" "$HOME/bin/$base"
+done
+shopt -u nullglob
+
+# wofi
+mkdir -p "$HOME/.config/wofi"
+link_one "$DOTFILES_DIR/wofi_config" "$HOME/.config/wofi/config"
+
 # TODO look into using hammerspoon again
-#ln hammerspoon.lua ~/.hammerspoon/init.lua
-mkdir -p ~/.config/nvim
-ln -s nvim_init.lua ~/.config/nvim/init.lua
+# link_one "$DOTFILES_DIR/hammerspoon.lua" "$HOME/.hammerspoon/init.lua"
 
-ln -s ./bin/* ~/bin/
 
-# Use hammerspoon on Mac. See Powertoys config in README for Windows
-if [ "$(uname)" == "Darwin" ]; then
-    mkdir ~/.hammerspoon
-    ln init.lua ~/.hammerspoon/init.lua
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Installs vim plug (need to install manually on other platforms)
     # Still need to call :PlugInstall inside of nvim to get all dependencies
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
-
-mkdir -p ~/.config/wofi
-ln -s wofi_config ~/.config/wofi/config
 
