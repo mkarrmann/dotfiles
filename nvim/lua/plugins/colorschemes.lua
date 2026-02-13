7775c29767625ea2067db9464ac96e0195d6225c
Matt Karrmann <mkarrmann@meta.com>
2026-02-13 14:07:33 -0800

    midnight color scheme

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"dasupradyumna/midnight.nvim",
+		name = "midnight",
+		lazy = false,
+		priority = 1000,
+	},
+
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "midnight",
+		},
+	},
+}
