099ccc57a74aaa94bc31645f6ec5ebfcb85dd99b
Matt Karrmann <mkarrmann@meta.com>
2026-02-16 10:12:19 -0800

    avoid crashing in specific remote nvim scenarios

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -9,6 +9,26 @@ return {
 		"amitds1997/remote-nvim.nvim",
 		cond = not vim.g.vscode,
 		dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
-		opts = {},
+		opts = {
+			client_callback = function(port, workspace_config)
+				if vim.env.TMUX then
+					local cmd = ("tmux new-window -n 'Remote: %s' 'nvim --server localhost:%s --remote-ui'")
+						:format(workspace_config.host, port)
+					vim.fn.jobstart(cmd, { detach = true })
+				else
+					require("remote-nvim.ui").float_term(
+						("nvim --server localhost:%s --remote-ui"):format(port),
+						function(exit_code)
+							if exit_code ~= 0 then
+								vim.notify(
+									("Remote UI exited with code %d"):format(exit_code),
+									vim.log.levels.ERROR
+								)
+							end
+						end
+					)
+				end
+			end,
+		},
 	},
 }
