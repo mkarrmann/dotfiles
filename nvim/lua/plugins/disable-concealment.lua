9c3ca89689d3cfc5494e87ae2f4f4205d1e5d072
Matt Karrmann <mkarrmann@meta.com>
2026-02-16 09:38:52 -0800

    update nvim-mini plugin full name

--- nvim/lua/plugins/disable-concealment.lua
+++ nvim/lua/plugins/disable-concealment.lua
@@ -15,7 +15,7 @@ return {
 
 	-- Keep mini.pairs but disable backtick auto-pairing
 	{
-		"echasnovski/mini.pairs",
+		"nvim-mini/mini.pairs",
 		opts = function(_, opts)
 			-- Disable backtick pairing by setting it to nil
 			opts.mappings = opts.mappings or {}
