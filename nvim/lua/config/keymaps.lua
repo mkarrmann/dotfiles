461b849c3c51502f419e9037edd7b652f3f91ec7
Matt Karrmann <mkarrmann@meta.com>
2026-02-17 12:51:50 -0800

    Improved neovim navigation in tmerminal mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -29,3 +29,8 @@ if vim.g.vscode then
 	vim.api.nvim_set_keymap('n', '<C-w>gd', "<Cmd>lua require('vscode').call('editor.action.peekDefinition')<CR>",
 		{ noremap = true, silent = true })
 end
+
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Go to Left Window" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Go to Lower Window" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Go to Upper Window" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Go to Right Window" })
