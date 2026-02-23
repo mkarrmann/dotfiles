d5d617cf78738e0f1c46e107155c6ec4589cb0be
Matt Karrmann <mattkarrmann@gmail.com>
2026-02-22 19:15:48 -0800

    more native codex integration

---
+++ nvim/lua/plugins/codex.lua
@@ -0,0 +1,49 @@
+return {
+	{
+		"pittcat/codex.nvim",
+		keys = {
+			{ "<leader>ao", "<cmd>CodexToggle<cr>", desc = "Toggle Codex" },
+			{ "<leader>aO", "<cmd>CodexOpen<cr>", desc = "Open Codex" },
+			{ "<leader>aP", "<cmd>CodexSendPath<cr>", desc = "Add current file to Codex" },
+			{ "<leader>aS", ":'<,'>CodexSendSelection<cr>", mode = "v", desc = "Send selection to Codex" },
+			{ "<leader>aR", ":'<,'>CodexSendReference<cr>", mode = "v", desc = "Send reference to Codex" },
+			{
+				"<leader>aS",
+				"<cmd>CodexSendPath<cr>",
+				ft = { "NvimTree", "neo-tree", "oil" },
+				desc = "Add file to Codex",
+			},
+		},
+		opts = {
+			terminal = {
+				provider = "auto",
+				direction = "vertical",
+				position = "right",
+				size = 0.35,
+			},
+			terminal_bridge = {
+				auto_attach = true,
+				path_format = "rel",
+				path_prefix = "@",
+				selection_mode = "reference",
+			},
+			extra_args = {
+				"--no-alt-screen",
+			},
+			env = {
+				EDITOR = vim.fn.expand("~/bin/nvim-edit-in-tab"),
+			},
+		},
+		init = function()
+			vim.api.nvim_create_user_command("CodexAdd", function()
+				vim.cmd("CodexSendPath")
+			end, { desc = "Add current file path to Codex" })
+			vim.api.nvim_create_user_command("CodexSend", function()
+				vim.cmd("'<,'>CodexSendSelection")
+			end, { range = true, desc = "Send visual selection to Codex" })
+			vim.api.nvim_create_user_command("CodexTreeAdd", function()
+				vim.cmd("CodexSendPath")
+			end, { desc = "Add current tree file path to Codex" })
+		end,
+	},
+}
