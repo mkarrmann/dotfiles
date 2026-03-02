253411c69b71c6d625a3f6fedfea7c07e0621774
Matt Karrmann <mkarrmann@meta.com>
2026-03-02 06:57:36 -0800

    diff split darker green

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -8,7 +8,7 @@ return {
 			vim.api.nvim_create_autocmd("ColorScheme", {
 				pattern = "midnight",
 				callback = function()
-					vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#2a5a38" })
+					vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#1f4a2d" })
 					vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#5c2530" })
 					vim.api.nvim_set_hl(0, "DiffChange", { bg = "#4d4418" })
 					vim.api.nvim_set_hl(0, "DiffText", { bg = "#6b5f20" })
