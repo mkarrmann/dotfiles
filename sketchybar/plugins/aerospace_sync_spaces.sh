f6509d4f1b7983e7a0de846ba0c46191ec599642
Matt Karrmann <mkarrmann@meta.com>
2026-03-19 18:08:31 -0700

    better multi-monitor sketchybar + aerospace sync

--- sketchybar/plugins/aerospace_sync_spaces.sh
+++ sketchybar/plugins/aerospace_sync_spaces.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 FOCUSED=$(aerospace list-workspaces --focused)
+VISIBLE=$(aerospace list-workspaces --monitor all --visible)
 
 aerospace list-workspaces --all \
     --format '%{workspace}%{tab}%{monitor-appkit-nsscreen-screens-id}' |
@@ -9,6 +10,13 @@ aerospace list-workspaces --all \
             sketchybar --set "space.$sid" \
                 associated_display="$display" \
                 background.drawing=on \
+                background.color=0xff89b4fa \
+                label.color=0xffffffff
+        elif echo "$VISIBLE" | grep -qx "$sid"; then
+            sketchybar --set "space.$sid" \
+                associated_display="$display" \
+                background.drawing=on \
+                background.color=0xff585b70 \
                 label.color=0xffffffff
         else
             sketchybar --set "space.$sid" \
