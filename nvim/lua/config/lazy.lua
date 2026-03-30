397d8cc48890c6047db354e8aaf8adc01ac326cb
Matt Karrmann <mkarrmann@meta.com>
2026-03-29 22:29:20 -0700

    enable nvim markdown rendering

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -7,6 +7,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lsp.none-ls" },
 		{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
 		{ import = "lazyvim.plugins.extras.ai.claudecode" },
+		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "plugins" },
 	},
 	defaults = {
