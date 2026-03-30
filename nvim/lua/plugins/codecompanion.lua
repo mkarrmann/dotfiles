d410e9edd810488ab23731adc5df2f5fbb33819c
Matt Karrmann <mkarrmann@meta.com>
2026-03-30 15:41:25 -0700

    code companion: inline indicators

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "franco-ruggeri/codecompanion-spinner.nvim",
     },
     opts = function()
       local has_snacks = pcall(require, "snacks")
@@ -35,6 +36,10 @@ return {
           log_level = "ERROR",
         },
 
+        extensions = {
+          spinner = {},
+        },
+
         display = {
           action_palette = {
             provider = has_snacks and "snacks" or "telescope",
@@ -53,6 +58,57 @@ return {
 
     config = function(_, opts)
       require("codecompanion").setup(opts)
+
+      local ns = vim.api.nvim_create_namespace("codecompanion_inline_indicator")
+      local spinner_frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+      local indicator = {}
+
+      local function clear()
+        if indicator.timer then
+          indicator.timer:stop()
+          indicator.timer:close()
+        end
+        if indicator.bufnr and vim.api.nvim_buf_is_valid(indicator.bufnr) then
+          vim.api.nvim_buf_clear_namespace(indicator.bufnr, ns, 0, -1)
+        end
+        indicator = {}
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "CodeCompanionRequestStarted",
+        callback = function()
+          local bufnr = vim.api.nvim_get_current_buf()
+          if vim.bo[bufnr].filetype == "codecompanion" then return end
+
+          clear()
+          local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+          indicator.bufnr = bufnr
+
+          local frame = 0
+          indicator.timer = vim.uv.new_timer()
+          indicator.timer:start(0, 80, function()
+            vim.schedule(function()
+              if not vim.api.nvim_buf_is_valid(bufnr) then
+                clear()
+                return
+              end
+              pcall(vim.api.nvim_buf_clear_namespace, bufnr, ns, 0, -1)
+              pcall(vim.api.nvim_buf_set_extmark, bufnr, ns, line, 0, {
+                virt_text = { { spinner_frames[frame + 1] .. " Processing…", "Comment" } },
+                virt_text_pos = "eol",
+              })
+              frame = (frame + 1) % #spinner_frames
+            end)
+          end)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "CodeCompanionRequestFinished",
+        callback = function()
+          clear()
+        end,
+      })
     end,
 
     cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
