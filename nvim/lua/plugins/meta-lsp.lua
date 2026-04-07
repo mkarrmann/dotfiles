0e478cd1c8fb8b11cbdcc58e27220995c881f09a
Matt Karrmann <mkarrmann@meta.com>
2026-04-07 13:13:12 -0700

    something with buck and something with integration myles with telescope

--- nvim/lua/plugins/meta-lsp.lua
+++ nvim/lua/plugins/meta-lsp.lua
@@ -14,6 +14,7 @@ return {
 				"thriftlsp@meta",
 				"rust-analyzer@meta",
 				"linttool@meta",
+				"buck2@meta",
 				"hhvm",
 			})
 
@@ -23,6 +24,25 @@ return {
 				meta.null_ls.formatting.arclint,
 			})
 		end,
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{
+				"<leader>p",
+				function()
+					local meta_util = require("meta.util")
+					if meta_util.arc.get_project_root(vim.uv.cwd()) then
+						require("telescope").load_extension("myles")
+						vim.cmd("Telescope myles")
+					else
+						require("lazyvim.util").pick().telescope("files")()
+					end
+				end,
+				desc = "Find Files (Myles)",
+			},
+		},
 	},
 
 	{
